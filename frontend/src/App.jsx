c6f4a2c1266721fa51bf8bed2b8c3a14d64cb4d4
Prince Mishra <mrprimi91@gmail.com>
2026-01-04 13:56:14 +0530

    case sensitive, frontend ready, deploying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login";
